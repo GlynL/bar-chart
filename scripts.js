397c9bbac6fd45e63621a4f6d680228ce74786c1
Glyn L <glyn_92@hotmail.com>
2018-06-05 13:19:15 +0700

    completed

--- scripts.js
+++ scripts.js
@@ -9,10 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function barChart(data) {
-    const height = 600;
+    const height = 800;
     const width = 1400;
-    const padding = 40;
-    const barPadding = 1;
+    const padding = 80;
     const barCount = data.length;
     const barWidth = (width - padding * 2) / barCount;
 
@@ -31,9 +30,8 @@ document.addEventListener("DOMContentLoaded", () => {
       .domain([0, d3.max(data, d => d[1])])
       .range([height - padding, padding]);
 
+    // y-axis
     const yAxis = d3.axisLeft(yScale);
-
-    console.log(xScale(new Date("2001, 01, 01")));
 
     svg
       .append("g")
@@ -41,25 +39,75 @@ document.addEventListener("DOMContentLoaded", () => {
       .attr("id", "y-axis")
       .call(yAxis);
 
+    // x-axis
+    const xAxis = d3.axisBottom(xScale);
+
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height - padding})`)
+      .attr("id", "x-axis")
+      .call(xAxis);
+
+    // tooltip
+    const tooltip = d3
+      .select("body")
+      .append("div")
+      .attr("id", "tooltip")
+      .text("a simple tooltip");
+
+    // plotting data
     svg
       .selectAll("rect")
       .data(data)
       .enter()
       .append("rect")
-      .attr("x", (d, i) => xScale(d[0]))
-      .attr("y", d => height - padding - yScale(d[1]))
+      .attr("x", (d, i) => barWidth * i + padding)
+      .attr("y", d => yScale(d[1]))
       .attr("width", barWidth)
-      .attr("height", d => yScale(d[1]))
-      .attr("fill", "navy");
+      .attr("height", d => height - padding - yScale(d[1]))
+      .classed("bar", true)
+      .attr("data-date", d => d[0])
+      .attr("data-gdp", d => d[1])
+      .attr("fill", "navy")
+      .on("mouseover", d => {
+        tooltip
+          .style("visibility", "visible")
+          .style("top", event.pageY - 30 + "px")
+          .style("left", event.pageX + 10 + "px")
+          .text(`${d[0]} GDP was $${d[1]}`)
+          .attr("data-date", d[0]);
+      })
+      .on("mouseout", d => tooltip.style("visibility", "hidden"));
 
+    // title
     svg
       .append("text")
       .text("Bar Chart")
       .attr("x", width / 2)
       .attr("y", padding)
-      // .attr("dy", "1em")
+      .attr("dy", "-1em")
       .attr("text-anchor", "middle")
       .attr("id", "title")
       .style("font-size", "2em");
+
+    // x-axis label
+    svg
+      .append("text")
+      .text("Year")
+      .attr("x", width / 2)
+      .attr("y", height - padding)
+      .attr("dy", "3em")
+      .attr("text-anchor", "middle")
+      .attr("id", "x-label")
+      .style("font-size", "1.5em");
+
+    // y-axis label
+    svg
+      .append("text")
+      .text("GDP (b)")
+      .attr("transform", "rotate(-90)")
+      .attr("y", "1em")
+      .attr("x", -height / 2)
+      .style("font-size", "1.5em");
   }
 });
